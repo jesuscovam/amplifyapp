5071975fd4b9cdda253ed99f1386f3e596c3ceb8
jesusacm <jesus@dulink.net>
2020-07-12 08:42:05 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Rob did it, he knows 2 much. f him.
         </p>
         <a
           className="App-link"
